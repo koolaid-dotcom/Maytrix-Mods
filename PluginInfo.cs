248cbdc6494419dd99010711c0488700e2de28ff
koolaid-dotcom <jkircher128@gmail.com>
2026-08-19 15:54:12 -0400

    Add PluginInfo.cs

---
+++ PluginInfo.cs
@@ -0,0 +1,14 @@
+namespace MaytrixMods
+{
+    internal static class PluginInfo
+    {
+        public const string Guid = "com.maytrixmods.menu";
+        public const string Name = "Maytrix Mod Menu";
+        public const string Version = "0.1.0";
+
+        // Replace this with a public discord.gg invite when one is available.
+        public const string DiscordUrl = "https://discord.com/channels/1520945955375943822";
+        public const string RepositoryUrl = "https://github.com/koolaid-dotcom/Maytrix-Mods";
+    }
+}
+
